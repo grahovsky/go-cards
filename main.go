f339c6faeb69f2e8687f453ea7a735b66a919860
Grakhovsky <grahovsky@gmail.com>
2022-11-22 08:26:17 +0300

    variables

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// var card string = "Ace of Speads"
+	card := "Ace of Speads"
+	card = "Five of Diamonds"
+	fmt.Println(card)
+
+	// var card1 = true
+	// fmt.Println(reflect.TypeOf(card1))
+
+}
